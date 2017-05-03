19723448904c65fb26154fec3f0082a199c0db1a
Aleksandr Belov <alwh2004@gmail.com>
2017-05-03 17:37:31 +0300

    first commit

---
+++ app/src/main/java/com/alwh/alweather/json/Coord.java
@@ -0,0 +1,16 @@
+
+package com.alwh.alweather.json;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Coord {
+
+    @SerializedName("lon")
+    @Expose
+    public Double lon;
+    @SerializedName("lat")
+    @Expose
+    public Double lat;
+
+}
