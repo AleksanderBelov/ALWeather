965f4a6ca0f7b547e3c06f2b067c845b6abf21bd
Aleksandr Belov <alwh2004@gmail.com>
2017-05-12 17:32:41 +0300

    alpha version

--- app/src/main/java/com/alwh/alweather/UI/MainActivity.java
+++ app/src/main/java/com/alwh/alweather/UI/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.FragmentTransaction;
 
 import android.view.View;
 import android.widget.TextView;
@@ -13,11 +15,17 @@ import android.widget.TextView;
 
 import com.alwh.alweather.R;
 import com.alwh.alweather.helpers.AppRoot;
+import com.alwh.alweather.json.forecast.Weather;
 import com.alwh.alweather.model.ControlService;
 
 public class MainActivity extends AppCompatActivity {
 
+
     final String TAG = "AlWeather/MActivity: ";
+   WeatherFragment weatherFragment;
+   ForecastFragment forecastFragment;
+
+    FragmentTransaction fragmentTransaction;
 
 
 
@@ -28,11 +36,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        weatherFragment = new WeatherFragment();
+        forecastFragment = new ForecastFragment();
+
+        fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.frame_conteiner, weatherFragment);
+        fragmentTransaction.commit();
+
+        fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame_conteiner, forecastFragment);
+        fragmentTransaction.commit();
 
 
-        Intent startActivityIntent = new Intent(MainActivity.this, WeatherActivity.class);
-        startActivity(startActivityIntent);
-        MainActivity.this.finish();
+
+
+
+
+        //       Intent startActivityIntent = new Intent(MainActivity.this, WeatherActivity.class);
+ //       startActivity(startActivityIntent);
+ //       MainActivity.this.finish();
 
 
     }
